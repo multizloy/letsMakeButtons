a3f1d8f2279fdd7c43b22dd12e4e411c15f16d21
Renat Shabanov <multizloy@gmail.com>
2024-08-04 11:07:24 +0300

    class button

--- main.py
+++ main.py
@@ -9,9 +9,34 @@ pygame.init()
 fps = 60
 fps_Clock = pygame.time.Clock()
 width, height = 640, 480
-screen = pygame.display.set_mode((width,height))
+screen = pygame.display.set_mode((width, height))
 
 font = pygame.font.SysFont("Arial", 40)
 
 object = []
 
+
+class Button:
+    def __init__(
+        self,
+        x,
+        y,
+        width,
+        height,
+        button_text="Button",
+        onclick_function=None,
+        one_press=False,
+    ):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.onclick_function = onclick_function
+        self.one_press = one_press
+        self.already_pressed = False
+
+        self.fill_colors = {
+            "normal": "#ffffff",
+            "hover": "#666666",
+            "pressed": "#333333",
+        }
