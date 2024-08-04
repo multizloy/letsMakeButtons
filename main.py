b28ae69c60413a918b6572deb4d6d5002feadc7d
Renat Shabanov <multizloy@gmail.com>
2024-08-04 10:57:54 +0300

    setup

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
-#imports
+# imports
 
 import sys
 import pygame
+
+# configuration
+
+pygame.init()
+fps = 60
+fps_Clock = pygame.time.Clock()
+width, height = 640, 480
+screen = pygame.display.set_mode((width,height))
+
+font = pygame.font.SysFont("Arial", 40)
+
+object = []
+
