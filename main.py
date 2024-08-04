429350af9f35393fe942b87289a4d2bb20019b1e
Renat Shabanov <multizloy@gmail.com>
2024-08-04 11:22:31 +0300

    process function

--- main.py
+++ main.py
@@ -47,3 +47,27 @@ class Button:
         self.button_surf = font.render(button_text, True, (20, 20, 20))
 
         objects.append(self)
+
+    def process(self):
+        mouse_pos = pygame.mouse.get_pos()
+        self.button_surface.fill(self.fill_colors["normal"])
+        if self.button_rect.collidepoint(mouse_pos):
+            self.button_surface.fill(self.fill_colors["hover"])
+            if pygame.mouse.get_pressed(num_buttons=3)[0]:
+                self.button_surface.fill(self.fill_colors["pressed"])
+                if self.one_press:
+                    self.onclick_function()
+                elif not self.already_pressed:
+                    self.onclick_function()
+                    self.already_pressed = True
+            else:
+                self.already_pressed = False
+
+        self.button_surface.blit(
+            self.button_surf,
+            [
+                self.button_rect.width / 2 - self.button_surf.get_rect().width / 2,
+                self.button_rect.height / 2 - self.button_surf.get_rect().height / 2,
+            ],
+        )
+        screen.blit(self.button_surface, self.button_rect)
